060fd0e79ce2a0a7c78dfd7d7f3e9710c30f58d2
Roland Chelwing <jolleee@hotmail.com>
2024-01-08 21:48:29 +0100

    Improvements to EnemySpawner

--- Assets/Scripts/Management/Spawner/EnemySpawner.cs
+++ Assets/Scripts/Management/Spawner/EnemySpawner.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : Singleton<EnemySpawner>
@@ -9,46 +8,33 @@ public class EnemySpawner : Singleton<EnemySpawner>
 	const int StartMinPoints = 20;
 	const int StartMaxPoints = 20;
 	const int TotalSpawnsPerLevel = 5;
+	const int GroupOffsetDistanceMax = 3;
 
 	[HideInInspector] public bool IsSpawning;
 	[HideInInspector] public Tower Target;
 
-	public int MinRadius = 30;
-	public int MaxRadius = 40;
-	public EnemyVariants EnemyVariants;
-	public LevelSpecificSpawns LevelSpecificSpawns;
+	[Header("Settings")]
+	[SerializeField] int _minRadius = 30;
+	[SerializeField] int _maxRadius = 40;
+	[SerializeField] EnemyVariants _enemyVariants;
+	[SerializeField] LevelSpecificSpawns _levelSpecificSpawns;
 
 	[Header("Debug")]
-	public bool ShowGizmos;
-
+	[SerializeField] bool _showGizmos;
 
 	float _nextSpawnTime;
 	float _timePerSpawn;
 	int _spawnedWavesCurrentLevel;
 	int _pointsRemainingFromLastSpawn;
-	readonly int _groupOffsetDistanceMax = 3;
 	Dictionary<int, LevelSpawnConfiguration> _levelSpawnConfigurations;
 
 	protected override void Awake()
 	{
 		base.Awake();
 
-		_levelSpawnConfigurations = new Dictionary<int, LevelSpawnConfiguration>();
-		foreach (var spawnOverride in LevelSpecificSpawns)
-		{
-			_levelSpawnConfigurations[spawnOverride.Level] = spawnOverride;
-		}
+		InitializeLevelSpawnConfigurations();
 
 		GameController.OnLevelChanged += OnLevelChanged;
-	}
-
-
-	void InitializeForLevel()
-	{
-		_timePerSpawn = GameController.Instance.TimePerLevel / TotalSpawnsPerLevel;
-		CalculateNextSpawnTime(true);
-		IsSpawning = true;
-		_spawnedWavesCurrentLevel = 0;
 	}
 
 	void Update()
@@ -56,13 +42,25 @@ public class EnemySpawner : Singleton<EnemySpawner>
 		if (IsSpawning && Time.time >= _nextSpawnTime)
 		{
 			TrySpawn();
-			CalculateNextSpawnTime();
+			_nextSpawnTime += _timePerSpawn;
 		}
 	}
 
-	void CalculateNextSpawnTime(bool levelStart = false)
+	void InitializeLevelSpawnConfigurations()
 	{
-		_nextSpawnTime = levelStart ? Time.time : _nextSpawnTime + _timePerSpawn;
+		_levelSpawnConfigurations = new Dictionary<int, LevelSpawnConfiguration>();
+		foreach (var spawnOverride in _levelSpecificSpawns)
+		{
+			_levelSpawnConfigurations[spawnOverride.Level] = spawnOverride;
+		}
+	}
+
+	void InitializeForLevel()
+	{
+		_timePerSpawn = GameController.Instance.TimePerLevel / TotalSpawnsPerLevel;
+		_nextSpawnTime = Time.time;
+		IsSpawning = true;
+		_spawnedWavesCurrentLevel = 0;
 	}
 
 	void TrySpawn()
@@ -104,16 +102,6 @@ public class EnemySpawner : Singleton<EnemySpawner>
 		SpawnGroup(pointsForThisSpawn);
 	}
 
-	LevelSpawnConfiguration? GetLevelSpecificSpawn(int currentLevel)
-	{
-		if (_levelSpawnConfigurations.TryGetValue(currentLevel, out var spawnOverride))
-		{
-			return spawnOverride;
-		}
-
-		return null;
-	}
-
 	void SpawnGroup(int points)
 	{
 		if (points <= 0)
@@ -139,16 +127,36 @@ public class EnemySpawner : Singleton<EnemySpawner>
 		}
 	}
 
+	void SpawnEnemy(GameObject prefab, Vector3 groupSpawnPosition)
+	{
+		// Slight random offset from the group's central spawn position
+		var offset = new Vector3(RandomManager.Enemy(-GroupOffsetDistanceMax, GroupOffsetDistanceMax), 0, RandomManager.Enemy(-GroupOffsetDistanceMax, GroupOffsetDistanceMax)); // Offset range can be adjusted
+		var spawnPosition = groupSpawnPosition + offset;
+		var rotation = Quaternion.LookRotation(transform.position - spawnPosition, Vector3.up);
+
+		Instantiate(prefab, spawnPosition, rotation);
+	}
+
+	LevelSpawnConfiguration? GetLevelSpecificSpawn(int currentLevel)
+	{
+		if (_levelSpawnConfigurations.TryGetValue(currentLevel, out var spawnOverride))
+		{
+			return spawnOverride;
+		}
+
+		return null;
+	}
+
 	Vector3 GetRandomSpawnPosition()
 	{
 		var randomDirection = RandomManager.InsideUnitCircleNormalized();
-		return transform.position + (new Vector3(randomDirection.x, 0, randomDirection.y) * RandomManager.Enemy(MinRadius, MaxRadius));
+		return transform.position + (new Vector3(randomDirection.x, 0, randomDirection.y) * RandomManager.Enemy(_minRadius, _maxRadius));
 	}
 
 	Enemy ChooseEnemyToSpawn(int remainingPoints)
 	{
 		// Filter enemies that can be spawned within the remaining points
-		var possibleEnemies = Array.FindAll(EnemyVariants.Enemies, e => e.Value <= remainingPoints);
+		var possibleEnemies = Array.FindAll(_enemyVariants.Enemies, e => e.Value <= remainingPoints);
 		if (possibleEnemies.Length == 0)
 		{
 			return null;
@@ -157,24 +165,12 @@ public class EnemySpawner : Singleton<EnemySpawner>
 		return possibleEnemies[RandomManager.Enemy(0, possibleEnemies.Length)];
 	}
 
-	void SpawnEnemy(GameObject prefab, Vector3 groupSpawnPosition)
-	{
-		// Slight random offset from the group's central spawn position
-		var offset = new Vector3(RandomManager.Enemy(-_groupOffsetDistanceMax, _groupOffsetDistanceMax), 0, RandomManager.Enemy(-_groupOffsetDistanceMax, _groupOffsetDistanceMax)); // Offset range can be adjusted
-		var spawnPosition = groupSpawnPosition + offset;
-		var rotation = Quaternion.LookRotation(transform.position - spawnPosition, Vector3.up);
-
-		Instantiate(prefab, spawnPosition, rotation);
-	}
-
-
 	(int minPoints, int maxPoints) CalculatePointsForLevel(int level)
 	{
 		var minPoints = StartMinPoints + (PointLevelScaling * (level - 1));
 		var maxPoints = StartMaxPoints + (PointLevelScaling * (level - 1));
 		return (minPoints, maxPoints);
 	}
-
 
 	protected override void OnDestroy()
 	{
@@ -184,15 +180,15 @@ public class EnemySpawner : Singleton<EnemySpawner>
 
 	void OnDrawGizmos()
 	{
-		if (!ShowGizmos)
+		if (!_showGizmos)
 		{
 			return;
 		}
 		Gizmos.color = Color.red;
 
 		// Draw circles for min and max radius, not spheres
-		GizmosExtras.Draw2dCircle(transform.position, MinRadius);
-		GizmosExtras.Draw2dCircle(transform.position, MaxRadius);
+		GizmosExtras.Draw2dCircle(transform.position, _minRadius);
+		GizmosExtras.Draw2dCircle(transform.position, _maxRadius);
 	}
 
 	void OnLevelChanged()
