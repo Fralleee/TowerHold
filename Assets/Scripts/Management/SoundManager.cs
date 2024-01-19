8328d36f5fb3772c296ff501ff079bd322c7b358
Roland Chelwing <jolleee@hotmail.com>
2024-01-19 17:01:08 +0100

    UI overlay shader

--- Assets/Scripts/Management/SoundManager.cs
+++ Assets/Scripts/Management/SoundManager.cs
@@ -13,9 +13,11 @@ public class SoundManager : SerializedSingleton<SoundManager>
 	int _currentTrackIndex = 0;
 	bool _isPlayingMusic = false;
 	readonly float _fadeDuration = 1.5f;
+	float _musicVolume;
 
 	void Start()
 	{
+		_musicVolume = _musicSource.volume;
 		_currentPlaylist = _playList;
 		PlayMusic();
 	}
@@ -35,6 +37,7 @@ public class SoundManager : SerializedSingleton<SoundManager>
 		if (_currentPlaylist.Count > 0)
 		{
 			_isPlayingMusic = true;
+			_currentTrackIndex = UnityEngine.Random.Range(0, _currentPlaylist.Count);
 			_musicSource.clip = _currentPlaylist[_currentTrackIndex];
 			StartCoroutine(FadeIn(_musicSource, _fadeDuration));
 		}
@@ -76,9 +79,10 @@ public class SoundManager : SerializedSingleton<SoundManager>
 		audioSource.volume = 0;
 		audioSource.Play();
 
-		while (audioSource.volume < 1)
+		while (audioSource.volume < _musicVolume)
 		{
 			audioSource.volume += Time.deltaTime / duration;
+			audioSource.volume = Mathf.Clamp(audioSource.volume, 0, _musicVolume);
 			yield return null;
 		}
 	}
