f048a67d0d5ff88936fbe021a6a74087ae5e88be
Roland Chelwing <jolleee@hotmail.com>
2024-04-24 13:41:26 +0200

    Add odin stuff

--- Assets/Scripts/Management/Enemy/EnemyVariants.cs
+++ Assets/Scripts/Management/Enemy/EnemyVariants.cs
@@ -1,7 +1,16 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemyVariants", menuName = "VAKT/Enemy/Spawner/EnemyVariants")]
 public class EnemyVariants : ScriptableObject
 {
+	[AssetList(CustomFilterMethod = "HasEnemyComponent")]
 	public Enemy[] Enemies;
+
+#pragma warning disable IDE0051 // Remove unused private members
+	bool HasEnemyComponent(GameObject obj)
+#pragma warning restore IDE0051 // Remove unused private members
+	{
+		return obj.GetComponent<Enemy>() != null;
+	}
 }
